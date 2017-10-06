6b81ebf5c863e8e8efa470ff6e20bc4ccbefb14d
mrizkan <rizkan@itmartx.net>
2017-10-06 10:21:57 +0530

    after set

---
+++ inc/header.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Eye with Us</title>
+
+    <meta name="description" content="EyeCare is a premium Eyecare template, which will ideally be suitable for any sites related to Optometrist, Eye Doctor, Medical,  LAsik Eye Surgery Center, Eye Hospital, Opticals, medicine, clinics, doctors, eye clinics and any other sites related to medical  topics or health care OR Eye Care" />
+    <meta name="keywords" content="Eye Care, Eye clinics, Ophthalmologist, Laser Vision, doctors, health, Vision Care, Eye hospital, medical, Optical, LAsik,  Optometrist, Optician, Eyecare Associates" />
+    <meta name="author" content="Accura  Themes" />
+
+    <!-- Google Fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700italic,700,900,900italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Raleway:400,800italic,800,900italic,900,700italic,700,600italic,600,500italic,500,400italic,300italic,300,200italic,200,100italic,100' rel='stylesheet' type='text/css'>
+
+    <!-- Style Sheets -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="css/animations.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/loader.css" type="text/css"/>
+
+    <!-- Menu Css -->
+    <link rel="stylesheet" href="css/bootstrap-menu.css" type="text/css">
+    <link rel="stylesheet" href="css/slicknav.css">
+    <!-- // Menu Css -->
+
+    <!-- Light Box Css -->
+    <link rel="stylesheet" href="css/ekko-lightbox.css">
+    <link rel="stylesheet" href="css/ekko-lightbox-dark-skin.css">
+    <!-- // Light Box Css -->
+
+    <!-- Revolution Slider -->
+    <link rel="stylesheet" type="text/css" href="css/settings.css">
+    <link rel="stylesheet" type="text/css" href="css/layers.css">
+    <link rel="stylesheet" type="text/css" href="css/navigation.css">
+    <link rel="stylesheet" type="text/css" href="css/slider-one-thumb.css" />
+    <!-- // Revolution Slider -->
+
+    <!-- Owl Slider Css -->
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.css">
+    <!-- // Owl Slider Css -->
+
+    <!-- BX Slider -->
+    <!--<link rel="stylesheet" type="text/css" href="css/jquery.bxslider.css" />-->
+    <!-- // BX Slider -->
+
+    <!-- Before After And Gallery Css -->
+    <link rel="stylesheet" href="css/portfolio.css">
+    <link rel="stylesheet" href="css/twentytwenty.css">
+    <!-- // Before After And Gallery Css -->
+
+    <!-- Custom Css -->
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/color.css">
+    <!-- Custom Css -->
+
+    <link rel="shortcut icon" href="images/favicon.png" />
+    <link rel="apple-touch-icon" href="images/apple_touch_icon.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="images/apple_touch_icon_72x72.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="images/apple_touch_icon_114x114.png" />
+
+    <script type="text/javascript" src="js/jquery-1.12.4.js"></script>
+
+</head>
+
+<body>
+
+<!--PRELOADER-->
+<section id="jSplash">
+    <div class="sk-spinner sk-spinner-three-bounce">
+        <div class="sk-bounce1"></div>
+        <div class="sk-bounce2"></div>
+        <div class="sk-bounce3"></div>
+    </div>
+</section>
+
+<!-- Wrapper
+=============================-->
+<div class="wrapper">
+
+    <!-- Header Section
+    =============================-->
+
+    <header id="header" class="grow-mode">
+        <!--  Top bar -->
+        <div class="top-bar top-bar-white">
+            <div class="container">
+                <div class="row">
+                    <!-- Left Section -->
+                    <div class="top-bar-left">
+                        <ul class="social-icons clearfix">
+                            <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Youtube"><i class="fa fa-youtube"></i></a></li>
+                            <li><a href="#" data-toggle="tooltip" data-placement="bottom" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                    <!-- Left Section -->
+                    <!-- Right Section -->
+                    <div class="top-bar-right">
+                        <ul class="right-top pull-left">
+                            <li><a href="#"><i class="fa fa-clock-o"></i>Mon - Fri : 09:00 - 18:30</a></li>
+                            <li><a href="#"><i class="fa fa-clock-o"></i>Mon - Fri : 09:00 - 18:30</a></li>
+                            <li><a href="http://www.tel.com"><i class="fa fa-phone"></i> +94 778 280 494</a></li>
+                        </ul>
+
+                    </div>
+                    <!-- Right Section -->
+                </div>
+            </div>
+            <span class="close fa fa-chevron-down"></span>    </div>
+        <!--  // Top bar -->
+
+        <!-- Menu Sections -->
+        <nav class="navbar navbar-default dropdown-white">
+            <div class="container">
+                <div class="menu clearfix">
+                    <div class="navbar-header">
+                        <a class="navbar-brand" href=""><img src="images/logo.png" alt=" " class="img-responsive center-block"></a>                </div>
+                    <div class="collapse navbar-collapse js-navbar-collapse text-center">
+                        <ul id="sub-nav" class="nav navbar-nav navbar-menu">
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Home </a>
+
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">About us </a>
+
+                            </li>
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Products </a>
+
+                            </li>
+
+
+
+                            <li>
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Contact Us </a>
+
+                            </li>
+                        </ul>
+                        <div class="navbar-right nav-right-center">
+                            <a href="#" class="btn btn-type1 btn-sm"> Make an Appointment</a>                  </div>
+
+                    </div><!-- /.nav-collapse -->
+                </div>
+            </div>
+            <!-- //Search Option -->
+        </nav>
+        <!-- Menu Sections -->
+    </header>
+
+    <!-- // Header Section
+    =============================-->
---
+++ products.php
@@ -0,0 +1,270 @@
+<?php include ('inc/header.php'); ?>
+
+
+
+    <!-- Services Section
+    ================================================== -->
+
+    <section class="top-bottom-spacing white-bg">
+        <div class="container">
+            <div class="row">
+                <div class="clearfix">
+                    <!-- Section 1 -->
+                    <div class="col-md-4 marbot10">
+                        <h1>
+                            <span class="fw-normal">We offer various </span>Eye Care &amp; Wear  <span class="fw-normal">Services</span>
+                        </h1>
+                    </div>
+                    <!-- // Section 1 -->
+
+                    <div class="col-md-8">
+                        <div class="row marbot20">
+                            <!-- Section 2 -->
+                            <div class="col-md-6">
+                                <p class="fontresize">
+                                    We offers a complete range of eye care services including LASIK and PRK refractive surgery, eye exams, vision testing for glasses and contacts.
+                                </p>
+                                <p class="fontresize">
+                                    All Laser Lasik, Cornea & Glaucoma, Laser Cataract Surgery, Brow lift services.
+                                </p>
+                            </div>
+                            <!-- // Section 2 -->
+
+                            <!-- Section 3 -->
+                            <div class="col-md-6">
+                                <p class="fontresize">
+                                    Cornea is the eye’s outermost layer. It is the clear, dome-shaped surface that covers the front of the eye.
+                                </p>
+                                <p class="fontresize">
+                                    Glaucoma is slowly damages the eyes and can causes vision loss.
+                                </p>
+                            </div>
+                            <!-- // Section 3 -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- // Services Section
+    ================================================== -->
+
+    <!-- Before after Section
+    ================================= -->
+
+    <section class="top-bottom-spacing grey-bg">
+    <div class="container">
+
+    <div id="options">
+        <ul id="filters" class="option-set clearfix" data-option-key="filter">
+            <li><a href="#" data-option-value="*" class="selected btn btn-type1-reverse">show all</a></li>
+            <li><a href="#" data-option-value=".laser-eye-surgery" class="btn btn-type1-reverse">Laser Eye Surgery</a></li>
+            <li><a href="#" data-option-value=".brown-lift" class="btn btn-type1-reverse">Eye Checkup</a></li>
+            <li><a href="#" data-option-value=".eye-care-center" class="btn btn-type1-reverse"> Eyecare Center</a></li>
+            <li><a href="#" data-option-value=".surgery" class="btn btn-type1-reverse">Surgery</a></li>
+        </ul>
+    </div>
+
+    <div id="portfolio" class="clearfix three-column marbot30">
+
+        <!-- Accura Item 1 -->
+        <div class="element transition laser-eye-surgery" data-category="laser-eye-surgery">
+
+            <a href="images/project01.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Comprehensive Eye Exams" data-footer="Optometrists and ophthalmologists use a wide variety of tests and procedures to examine your eyes">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/project01.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+        </div>
+        <!-- // Accura Item 1 -->
+
+        <!-- Accura Item 2 -->
+        <div class="element transition brown-lift" data-category="brown-lift">
+
+            <a href="images/project02.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Drooping Eyelids" data-footer="We reduced bagginess from her lower eyelids and made her feel young and better">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/project02.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+
+        </div>
+        <!-- // Accura Item 2 -->
+
+        <!-- Accura Item 3 -->
+        <div class="element transition eye-care-center" data-category="eye-care-center">
+
+            <a href="images/project06.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Steve John Lasik Patient quotes" data-footer="Very good service, Was very helpful, Doctor Steve responds promptly. I am very happy that i signed up to Accura Vision care service.">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/project06.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+
+        </div>
+        <!-- // Accura Item 3 -->
+
+        <!-- Accura Item 4 -->
+        <div class="element transition surgery" data-category="surgery">
+
+            <a href="images/project03.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Specialized Doctors" data-footer="Dr. Williams takes lots of time to explain how to manage your condition and understand how it affects your vision and your eyes">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/project03.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+
+        </div>
+        <!-- // Accura Item 4 -->
+
+        <!-- Accura Item 5 -->
+        <div class="element transition eye-care-center" data-category="eye-care-center">
+
+            <a href="images/multi-location-02.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Letterpress asymmetrical" data-footer="Chillwave hoodie ea gentrify aute sriracha consequat">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/multi-location-02.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+
+        </div>
+        <!-- // Accura Item 5 -->
+
+        <!-- Accura Item 6 -->
+        <div class="element transition brown-lift" data-category="brown-lift">
+
+            <a href="images/project04.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Specialized Doctors" data-footer="Dr. Williams takes lots of time to explain how to manage your condition and understand how it affects your vision and your eyes">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/project04.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+
+        </div>
+        <!-- // Accura Item 6 -->
+
+        <!-- Accura Item 7 -->
+        <div class="element transition laser-eye-surgery" data-category="laser-eye-surgery">
+
+            <a href="images/project05.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Letterpress asymmetrical" data-footer="Chillwave hoodie ea gentrify aute sriracha consequat">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/project05.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+
+        </div>
+        <!-- // Accura Item 7 -->
+
+        <!-- Accura Item 8 -->
+        <div class="element transition eye-care-center" data-category="eye-care-center">
+
+            <a href="images/multi-location-07.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Letterpress asymmetrical" data-footer="Chillwave hoodie ea gentrify aute sriracha consequat">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/multi-location-07.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+
+        </div>
+        <!-- // Accura Item 8 -->
+
+        <!-- Accura Item 9 -->
+        <div class="element transition surgery" data-category="surgery">
+
+            <a href="images/project06.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Letterpress asymmetrical" data-footer="Chillwave hoodie ea gentrify aute sriracha consequat">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/project06.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+
+        </div>
+        <!-- // Accura Item 9 -->
+
+        <!-- Accura Item 10 -->
+        <div class="element transition brown-lift" data-category="brown-lift">
+
+            <a href="images/multi-location-01.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Letterpress asymmetrical" data-footer="Chillwave hoodie ea gentrify aute sriracha consequat">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/multi-location-01.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+
+        </div>
+        <!-- // Accura Item 10 -->
+
+        <!-- Accura Item 11 -->
+        <div class="element transition eye-care-center" data-category="eye-care-center">
+
+            <a href="images/multi-location-03.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Letterpress asymmetrical" data-footer="Chillwave hoodie ea gentrify aute sriracha consequat">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/multi-location-03.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+
+        </div>
+        <!-- // Accura Item 11 -->
+
+        <!-- Accura Item 12 -->
+        <div class="element transition laser-eye-surgery" data-category="laser-eye-surgery">
+
+            <a href="images/multi-location-06.jpg" data-toggle="lightbox" data-gallery="accura-item1" data-title="Letterpress asymmetrical" data-footer="Chillwave hoodie ea gentrify aute sriracha consequat">
+
+                <div class="grid image-effect2">
+                    <figure>
+                        <img src="images/multi-location-06.jpg" alt=" " class="img-responsive" />
+                        <figcaption><i class="fa flaticon-symbols-1 gallery-icon transition"></i></figcaption>
+                    </figure>
+                </div>
+            </a>
+
+        </div>
+        <!-- // Accura Item 12 -->
+
+    </div> <!-- #portfolio -->
+    </div>
+    </section>
+    <!-- // Before after Section
+    ================================= -->
+
+
+
+<?php include ('inc/footer.php'); ?>
